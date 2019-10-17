c06013f14bcea76064221bd2fcafce46311438d3
Karyn Sieglitz <karyn.sieglitz@gmail.com>
2019-10-17 15:56:56 -0700

    start lambda-classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,46 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attr) {
+        this.name = attr.name;
+        this.age = attr.age;
+        this.location = attr.location
+    }
+    speak() {
+        return `Hello my name is ${this.name}, and I am from ${this.location}.`;
+    }
+}
+
+class Instructor extends Person {
+    constructor(insAttr) {
+        super(insAttr);
+        this.speciality = insAttr.specialty;
+        this.favLanguage;
+        this.catchPhrase
+    }
+    demo() {
+        return `Today we are learning about ${subject}.`;
+    }
+    grade() {
+        return `${student.name} receives a perfect scor on ${subject}`;
+    }
+}
+
+class Student extends Person {
+    constructor(studentAttr) {
+        super(studentAttr);
+        this.previousBackground = studentAttr.previousBackground;
+        this.className = studentAttr.className;
+        this.favSubjects = studentAttr.favSubjects
+    }
+    // listsSubjects() { //placeholder for now
+
+    // };
+    PRAssignment() {
+        return `${student.name} has submitted a PR for ${subject}`;
+    }
+
+    sprintChallenge() {
+        return `${student.name} has begun sprint challenge on ${subject}`;
+    }
+}
+
